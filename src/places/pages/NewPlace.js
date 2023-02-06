fe4c3cbc98ae4c2c1da982e5acf7132faae4f7ec
Septian Maulana <septianm028@gmail.com>
2023-02-06 16:09:24 +0700

    feat: add a custom form input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
+
 const NewPlace = () => {
-  return <div>NewPlace Works!</div>;
+  return (
+    <form className="place-form">
+      <Input element="input" type="text" label="Title" />
+    </form>
+  );
 };
 
 export default NewPlace;
